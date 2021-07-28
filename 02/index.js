ac28a7ffd661451aba85cb383dfc0bedd3b37043
oleg.lyamin <oleg.lyamin@gmail.com>
2021-07-28 20:42:28 +0300

    tada-2

--- 02/index.js
+++ 02/index.js
@@ -4,17 +4,19 @@ const { EventEmitter } = require('events');
 const emitter = new EventEmitter();
 
 class Handler {
-    static handle([str, stamp]) {
-        let dif = Math.floor((stamp - Date.now()) / 1000);
+    static handle(timer) {
+        let dif = Math.floor((timer.stamp - Date.now()) / 1000);
         if (dif > 0) {
             let s = dif % 60;
             let m = Math.floor((dif - s) / 60) % 60;
             let h = Math.floor((Math.floor((dif - s) / 60) - m) / 60) % 24;
             let d = Math.floor((Math.floor((Math.floor((dif - s) / 60) - m) / 60) - h) / 24);
-            console.log(`${d} days, ${h} hours, ${m} minutes, ${s} seconds until ${str}.`);
+            console.log(`${d} days, ${h} hours, ${m} minutes, ${s} seconds until ${timer.str}.`);
         }
-        else
-            console.log(`${str} is in the past.`)
+        else {
+            console.log(`${timer.str} is in the past.`)
+            timer.stop();
+        }
     }
 }
 
@@ -22,11 +24,17 @@ emitter.on('', Handler.handle);
 
 class Timer {
     constructor(str) {
+        this.str = str;
         let [hour, day, month, year] = str.split('-');
         this.stamp = new Date(year, month - 1, day, hour, 0, 0).getTime();
-        setInterval(() => { emitter.emit('', [str, this.stamp]) }, 1000);
+        this.interval = setInterval(() => { emitter.emit('', this) }, 1000);
+    }
+
+    stop = function () {
+        clearInterval(this.interval);
     }
 }
 
 let timer1 = new Timer("0-20-1-2022");
 let timer2 = new Timer("21-28-7-2021");
+let timer3 = new Timer("21-28-7-2020");
